769a6a01c90c55ea610d6add9e18d43efefc3a30
RyotaroSeto <hahahahawaiwai321@gmail.com>
2024-06-08 12:40:31 +0900

    commit

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	fmt.Println(foo())
 	fmt.Println(bar())
+	loop()
 }
 
 func foo() *int {
@@ -33,5 +34,21 @@ func NoEscape() int {
 func BenchmarkEscape(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = Escape()
+	}
+}
+
+func loop() {
+	for v := range 3 {
+		fmt.Println(&v) // エスケープされていたため、各イテレーションで異なるインスタンスが割り当てられる
+		// 0x140000a0030
+		// 0x140000a0038
+		// 0x140000a0040
+	}
+	for v := range 3 {
+		// 構造体をインターフェース変換してそのメソッドを呼ぶ実装がないため
+		println(&v) // エスケープされないため、ループ変数が使いまわされる
+		// 0x14000096eb8
+		// 0x14000096eb8
+		// 0x14000096eb8
 	}
 }
